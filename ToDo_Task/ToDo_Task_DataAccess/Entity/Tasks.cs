91a90d141ab2129beda35c4a471f877376616d84
Danial Shokouhmanesh <danialshokouhmanesh@outlook.com>
2023-05-10 23:31:16 +0330

    Feat: Created field changed

--- ToDo_Task/ToDo_Task_DataAccess/Entity/Tasks.cs
+++ ToDo_Task/ToDo_Task_DataAccess/Entity/Tasks.cs
@@ -6,5 +6,5 @@ public class Tasks
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public bool IsDone { get; set; }
-    public DateTime Created { get; set; }
+    public DateTime CreatedDate { get; set; }= DateTime.Now;
 }
---
+++ ToDo_Task/ToDo_Task_Migrations/Migrations/20230510195944_ChangeCratedDateField_Tasks.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo_Task_Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCratedDateField_Tasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "Tasks",
+                newName: "CreatedDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedDate",
+                table: "Tasks",
+                newName: "Created");
+        }
+    }
+}
